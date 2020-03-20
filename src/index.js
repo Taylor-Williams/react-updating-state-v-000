fd257d55b5bb048df6c5e7109d9140ff7d1d64c2
Taylor Williams <taylor@thewilliams.net>
2020-03-20 21:11:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick';
-ReactDOM.render(<ClickityClick>, document.getElementById('root'));
+ReactDOM.render(<ClickityClick />, document.getElementById('root'));
